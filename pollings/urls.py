f7b6044efb57fc8dcb3c0778a5c5bd10e464a721
Den Ermol <denisermolenko17@gmail.com>
2022-05-30 22:07:10 +0300

    some improvements

--- pollings/urls.py
+++ pollings/urls.py
@@ -3,10 +3,11 @@ from rest_framework_nested import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from pollings import views
+from pollings.views import RegisterView
 
 app_name = "pollings"
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'users', views.UserViewSet, basename="users")
 router.register(r'questions', views.QuestionViewSet, basename='questions')
 
@@ -14,8 +15,9 @@ questions_router = routers.NestedSimpleRouter(router, r'questions', lookup='ques
 questions_router.register(r'answers', views.AnswerViewSet, basename='question-answers')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(questions_router.urls)),
+    path('register', RegisterView.as_view(), name="register"),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh',  TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh',  TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+    path('', include(questions_router.urls))
 ]
--- pollings/views.py
+++ pollings/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from pollings.models import Question, Answer
@@ -22,9 +23,20 @@ def you_have_already_vote_response():
                     status=status.HTTP_400_BAD_REQUEST)
 
 
+class RegisterView(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
@@ -32,7 +44,7 @@ class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    @action(detail=True, url_path='is-already-vote')
+    @action(detail=True, url_path='is-already-vote', permission_classes=[IsAuthenticated])
     def is_already_vote(self, request, pk=None):
         question = self.get_object()
         jwt_object = JWTAuthentication()
