356154fbfc74b149c594a759f4b41295f39c1eb2
Denys Yermolenko <denisermolenko17@gmail.com>
2022-06-12 19:22:45 +0300

    lab 2 is done

--- pollings/consumers.py
+++ pollings/consumers.py
@@ -2,29 +2,52 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
 
 
 class PollingsConsumer(WebsocketConsumer):
+    online_users = []
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
-        self.group_name = "common"
+        self.common_group_name = "common"
+        self.admin_group_name = "admin"
 
     def connect(self):
         self.accept()
-        print(self.scope["user"])
-        print(self.channel_name)
         async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
+            self.common_group_name,
             self.channel_name
         )
+        user = self.scope["user"]
+        if type(user) is not AnonymousUser:
+            self.online_users.append(user.username)
+            async_to_sync(self.channel_layer.group_send)(
+                self.admin_group_name,
+                {
+                    "type": "added_new_user",
+                    "username": user.username,
+                }
+            )
+
+        if user.is_staff:
+            self.send(json.dumps(
+                {
+                    "type": "list_of_online_users",
+                    "users": self.online_users,
+                }
+            ))
+            async_to_sync(self.channel_layer.group_add)(
+                self.admin_group_name,
+                self.channel_name
+            )
 
     def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
 
         if data["type"] == "added_new_question":
             async_to_sync(self.channel_layer.group_send)(
-                self.group_name,
+                self.common_group_name,
                 {
                     "type": "added_new_question",
                     "message": data["message"],
@@ -33,7 +56,7 @@ class PollingsConsumer(WebsocketConsumer):
             )
         elif data["type"] == "deleted_question":
             async_to_sync(self.channel_layer.group_send)(
-                self.group_name,
+                self.common_group_name,
                 {
                     "type": "deleted_question",
                     "message": data["message"],
@@ -43,7 +66,7 @@ class PollingsConsumer(WebsocketConsumer):
             )
         elif data["type"] == "vote_for_answer":
             async_to_sync(self.channel_layer.group_send)(
-                self.group_name,
+                self.common_group_name,
                 {
                     "type": "vote_for_answer",
                     "message": data["message"],
@@ -54,11 +77,28 @@ class PollingsConsumer(WebsocketConsumer):
             )
 
     def disconnect(self, code):
-        print(self.scope["user"])
+        user = self.scope["user"]
         async_to_sync(self.channel_layer.group_discard)(
-            self.group_name,
+            self.common_group_name,
             self.channel_name
         )
+
+        if type(user) is not AnonymousUser:
+            if user.is_staff:
+                async_to_sync(self.channel_layer.group_discard)(
+                    self.admin_group_name,
+                    self.channel_name
+                )
+
+            self.online_users.remove(user.username)
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.admin_group_name,
+                {
+                    "type": "user_disconnected",
+                    "username": user.username,
+                }
+            )
 
     def added_new_question(self, event):
         if self.channel_name != event['sender_channel_name']:
@@ -71,3 +111,9 @@ class PollingsConsumer(WebsocketConsumer):
     def vote_for_answer(self, event):
         if self.channel_name != event['sender_channel_name']:
             self.send(text_data=json.dumps(event))
+
+    def added_new_user(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def user_disconnected(self, event):
+        self.send(text_data=json.dumps(event))
